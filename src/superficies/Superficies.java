0898100491e1cef8c4a6e7563627fdb1ba0db26a
Alex Urcera <Alex Urcera@Alex>
2015-06-01 20:29:52 +0200

    Commit

---
+++ src/superficies/Superficies.java
@@ -0,0 +1,30 @@
+package superficies;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import superficies.Visualizar;
+
+/**
+ * Examen de MVC 3ª Avaliación
+ * ramaCod examen
+ * @author damian
+ */
+public class Superficies {
+
+
+    /**cambios realizados
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //pedir fig
+        //Pedir dato
+        //calcularDato
+        //imrpimir
+        Modelo contenedor= new Modelo();
+        Visualizar.menu(contenedor);
+    }
+    
+    
+
+    
+    
+}
